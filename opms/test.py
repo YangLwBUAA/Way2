f4434d434c100ecc310a259fd8a77ac097684516
YangLwBUAA <924478750@qq.com>
2020-06-03 19:20:50 +0800

    11

---
+++ opms/test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/06/03 10:42:53
+@Author  :   YLw 
+@Version :   1.0
+@Contact :   924478750@qq.com
+'''
+# Start typing your code from here
+
+
+print('hello world')
+for i in range(10):
+    print(i+1)
+
+
+print('hello world')
+for i in range(10):
+    print(i+1)
+
+
+
+
