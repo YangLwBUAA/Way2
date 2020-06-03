f8442c62fd29d53db6f85507835daafaaed01743
YangLwBUAA <924478750@qq.com>
2020-06-03 19:10:41 +0800

    11

---
+++ new.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   new.py
+@Time    :   2020/06/03 19:10:25
+@Author  :   YLw 
+@Version :   1.0
+@Contact :   924478750@qq.com
+'''
+# Start typing your code from here
+
+
+print("dafasdf")
